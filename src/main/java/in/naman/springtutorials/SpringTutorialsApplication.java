7635e89fb628e9d18c1cfb3712a3ca1638a2d7d4
Naman Tamrakar <namantam1@gmail.com>
2024-07-27 15:05:27 +0530

    Add jwt api code

--- src/main/java/in/naman/springtutorials/SpringTutorialsApplication.java
+++ src/main/java/in/naman/springtutorials/SpringTutorialsApplication.java
@@ -7,39 +7,70 @@ import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
 import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.jwt.*;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class SpringTutorialsApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(SpringTutorialsApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(SpringTutorialsApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner runner(UserRepository userRepository, PasswordEncoder encoder) {
+        return args -> {
+            User user = new User();
+            user.username = "admin";
+            user.password = encoder.encode("admin");
+            var savedUser = userRepository.save(user);
+            log.info("user: {}", savedUser);
+        };
     }
 }
 
@@ -47,20 +78,29 @@ public class SpringTutorialsApplication {
 @Configuration
 class SecurityConfig {
     private @Value("${rsa.private-key}") RSAPrivateKey privateKey;
-    private @Value("${rsa.public-key}")RSAPublicKey publicKey;
+    private @Value("${rsa.public-key}") RSAPublicKey publicKey;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(AbstractHttpConfigurer::disable) // (1)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().authenticated() // (2)
+                        .requestMatchers("/auth/**").permitAll()
+                        .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // (3)
-                .httpBasic(Customizer.withDefaults()) // (4)
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        return username -> userRepository
+                .findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("user with username " + username + " not found!!")
+                );
     }
 
     @Bean
@@ -76,24 +116,20 @@ class SecurityConfig {
     }
 
     @Bean
-    public InMemoryUserDetailsManager users() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("admin")
-                        .password("{noop}admin")
-                        .authorities("read")
-                        .build()
-        );
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
 
+@RequiredArgsConstructor
 @Service
 class TokenService {
-
     private final JwtEncoder encoder;
-
-    public TokenService(JwtEncoder encoder) {
-        this.encoder = encoder;
-    }
 
     public String generateToken(Authentication authentication) {
         Instant now = Instant.now();
@@ -116,17 +152,69 @@ class TokenService {
 @RestController
 class HomeController {
     private final TokenService tokenService;
+    private final AuthenticationManager manager;
 
     @GetMapping
     String home() {
         return "home";
     }
 
-    @PostMapping("/token")
-    public String token(Authentication authentication) {
-        log.debug("Token requested for user: '{}'", authentication.getName());
-        String token = tokenService.generateToken(authentication);
-        log.debug("Token granted: {}", token);
+    @PostMapping("/auth/login")
+    public String login(@RequestBody Map<String, String> user) {
+        log.debug("User login: {}", user);
+        Authentication auth = manager.authenticate(
+                new UsernamePasswordAuthenticationToken(user.get("username"), user.get("password")));
+        String token = tokenService.generateToken(auth);
+        log.debug("JWT token: {}", token);
         return token;
     }
 }
+
+interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
+
+@ToString
+@Table("USERS")
+class User implements UserDetails {
+    @Id
+    Long id;
+    String username;
+    String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
